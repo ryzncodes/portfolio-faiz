34a9cde3c12f86bd99848f8bf4b434f3e2107aa8
Faiz <faizkasman@myra.com.my>
2024-10-23 18:48:33 +0800

    change wordings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,43 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
-import "./globals.css";
-import { ThemeProvider } from "./provider";
+import './globals.css';
+import { ThemeProvider } from './provider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+	title: 'Faiz Kasman | Full Stack Developer',
+	description: 'A Full Stack Developer from Malaysia.',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
-      </head>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang='en'
+			suppressHydrationWarning
+		>
+			<head>
+				<link
+					rel='icon'
+					href='/jsm-logo.png'
+					sizes='any'
+				/>
+			</head>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='dark'
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
